532687a6e1728d38564c3a4ca4c28881b55de407
Ramsaiharibabu <Haribabu91000@gmail.com>
2024-02-22 12:04:34 +0530

    con

--- db/conn.js
+++ db/conn.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-// const DB = process.env.DATABASE
-const DB = 'mongodb+srv://haribabu91000:wwnpperHUU2fotQK@cluster0.ykldvtg.mongodb.net/certificateGenerator?retryWrites=true&w=majority'
-mongoose.connect(DB,{
+const mongoose = require("mongoose");
+const DB = process.env.DATABASE;
+// const DB = 'mongodb+srv://haribabu91000:wwnpperHUU2fotQK@cluster0.ykldvtg.mongodb.net/certificateGenerator?retryWrites=true&w=majority'
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-
-}).then(()=>{
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log(`connection successful`);
-}).catch((err)=>
-console.log(err)
-);
+  })
+  .catch((err) => console.log(err));
